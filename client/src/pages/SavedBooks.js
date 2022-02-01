89ac4d19bbca6f6d61a46c21123b9820aa2968c9
DoodleBob <riley.foust18@gmail.com>
2022-01-31 21:07:45 -0500

    updated login and signup component

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -23,11 +23,9 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await removeBook({variables: {bookId}});
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      await removeBook({
+        variables: { bookId }
+      })
 
       removeBookId(bookId);
     } catch (err) {
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
+import { useMutation } from '@apollo/react-hooks';
+import { SAVED_BOOK } from '../utils/mutations';
+
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -14,6 +17,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBooks] = useMutation(SAVED_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -69,6 +74,7 @@ const SearchBooks = () => {
       const {data} = await saveBooks({
         variables: {bookData: {...bookToSave}}
       })
+      console.log(data);
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
