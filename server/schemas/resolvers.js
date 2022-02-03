2f21c48df396d5ce1cf6eb733b7bc136e4233fa0
DoodleBob <riley.foust18@gmail.com>
2022-02-02 22:57:24 -0500

    update

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -61,7 +61,7 @@ const resolvers = {
       if (context.user) {
         const updateUser = await User.findOneAndUpdate(
           {_id: context.user._id},
-          {$pull: {savedBooks: bookId}},
+          {$pull: {savedBooks: {bookId}}},
           {new: true}
         )
 
